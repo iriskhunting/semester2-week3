e05d5c587d6f46d128befcc0e7d58c36429986d6
iriskhunting <nlnp0745@leeds.ac.uk>
2026-02-16 20:37:08 +0000

    worksheet COMPLETE

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -11,27 +11,27 @@
 
     // define and initialise variables for the problem data 
     float salary = 30000;
-    float NIRate = 0;
-    float taxRate = 100;
+    float NIRate = 9;
+    float taxRate = 25;
     float finalSalary;
     float NIPaid;
+    float salaryAfterNI;
     float taxPaid;
 
     // calculate the deductions and final take-home salary
-    NIPaid = salary * (taxRate / 100);
-    finalSalary = salary - NIRate;
+    NIPaid = salary * (NIRate / 100);
+    salaryAfterNI = salary - NIPaid;
 
-    taxPaid = salary * (taxRate / 100);
-    finalSalary = salary - taxPaid;
+    taxPaid = (salaryAfterNI - 12500) * (taxRate / 100); //apply on taxable amount
+    if (taxPaid <= 0) {
+      taxPaid = 0;
+    }
 
-   if (finalSalary < 12500) {
-      taxPaid = finalSalary - 12500;
-      finalSalary = 12500;
-   }
+    finalSalary = salaryAfterNI - taxPaid;
 
     // Use only these print statement with appropriate formatting and variable names
     printf("Salary £%.2f\n",salary);
-    printf("NI contribution £%.2f\n",(salary * (NIRate / 100)));
+    printf("NI contribution £%.2f\n",NIPaid);
     printf("Tax contribution £%.2f\n",taxPaid);
     printf("Take home salary £%.2f\n",finalSalary);
 
